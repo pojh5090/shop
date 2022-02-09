9b29466d2331ab913b9c6b9a2240dc40f3b2df60
pojh5090 <pojh4@naver.com>
2022-02-10 01:11:59 +0900

    useEffect

--- src/Detail.js
+++ src/Detail.js
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components'
+import './Detail.scss';
+
+let 박스 = styled.div`
+    padding : 20px;
+`;
+
+let 제목 = styled.h4`
+    font-size : 25px;
+    color: ${props => props.색상}
+`
+
 
 function Detail(props) {
+
+    let [alert, alert변경] = useState(true);
+    let [inputData, inputData변경] = useState('');
+
+    // 컴포넌트가 등장, 업데이트 될 때 실행함 (2가지 경우)
+    useEffect(() => {
+        let 타이머 = setTimeout(() => { alert변경(false) }, 2000);
+        return () => { clearTimeout(타이머) }
+    }, []);
+
     let { id } = useParams();
     let 해당상품 = props.shoes.find(function (상품) {
         return 상품.id == id
@@ -10,6 +32,19 @@ function Detail(props) {
 
     return (
         <div className="container">
+            <박스>
+                <제목 className="red">Detail</제목>
+            </박스>
+            <input onChange={(e) => { inputData변경(e.target.value) }} />
+            {inputData}
+            {
+                alert == true
+                    ? (<div className="my-alert2">
+                        <p>재고가 얼마 남지 않았습니다.</p>
+                    </div>)
+                    : null
+            }
+
             <div className="row">
                 <div className="col-md-6">
                     <img src={'https://codingapple1.github.io/shop/shoes' + (해당상품.id + 1) + '.jpg'} width="100%" />
